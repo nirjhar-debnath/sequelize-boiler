181e45c366ed7742c9dbbef71966506045223392
nirjhar-debnath <100.nirjhar@gmail.com>
2022-08-19 18:36:15 +0530

    init

---
+++ migrations/20220819125507-create-post.js
@@ -0,0 +1,32 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, DataTypes) {
+    await queryInterface.createTable('posts', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER
+      },
+      uuid:{
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4
+      },
+      body: {
+        type: DataTypes.STRING,
+        allowNull: false 
+      },
+      createdAt: {
+        allowNull: false,
+        type: DataTypes.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: DataTypes.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('posts');
+  }
+};
